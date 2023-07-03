cf8b64102e5d0bf545b1be3bf4403efd89049eab
eduaardofranco <eduaardo.franco@gmail.com>
2023-07-03 09:17:00 +0100

    initial

---
+++ main.js
@@ -0,0 +1,58 @@
+
+// 1
+(function() {
+    //alert("Hello World!");
+})();
+// 2
+(function() {
+    let num1 = 10;
+    let num2 = 5;
+    let res = num1 + num2;
+    console.log(res);
+})();
+// 3
+(function() {
+    let num = 10;
+    console.log(typeof num == 'number' ? 'é numero' : 'não é número');
+})();
+//4
+(function() {
+    let str = '10';
+    console.log(typeof str == 'string' ? 'é string' : 'não é string');
+})();
+//5
+(function() {
+    let bol = false;
+    console.log(typeof bol == 'boolean' ? 'é boleano' : 'não é boleano');
+})();
+//6
+(function() {
+    let num1 = 10;
+    let num2 = 5;
+    let res = num1 - num2;
+    console.log(res);
+})();
+//7
+(function() {
+    let num1 = 10;
+    let num2 = 5;
+    let res = num1 * num2;
+    console.log(res);
+})();
+//8
+(function() {
+    let num1 = 10;
+    let num2 = 5;
+    let res = num1 / num2;
+    console.log(res);
+})();
+//9
+(function() {
+    let num = 30;
+    console.log( num % 2 == 0 ? 'é par' : 'não é par');
+})();
+//10
+(function() {
+    let num = 1;
+    console.log( num % 2 != 0 ? 'é impar' : 'não é impar');
+})();
